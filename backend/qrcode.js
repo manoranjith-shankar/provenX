9f49badf236ee9091b837709852a9f56b418ea8a
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-03-21 23:36:22 +0530

    addedQrCodeDir

--- backend/qrcode.js
+++ backend/qrcode.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 const qrcode = require('qrcode');
+const path = require('path');
 
 // Read CSV file
 const transactions = fs.readFileSync('transaction_hashes.csv', 'utf-8').trim().split('\n');
 
+// Create qrCodes directory if it doesn't exist
+const qrCodeDirectory = './qrCodes';
+if (!fs.existsSync(qrCodeDirectory)) {
+  fs.mkdirSync(qrCodeDirectory);
+}
+
 // Generate QR code for each transaction
 transactions.forEach((transaction, index) => {
-  qrcode.toFile(`transaction_${index}.png`, transaction, {
+  const qrCodeFilePath = path.join(qrCodeDirectory, `transaction_${index}.png`);
+  qrcode.toFile(qrCodeFilePath, transaction, {
     errorCorrectionLevel: 'H',
     width: 500,
     margin: 1
